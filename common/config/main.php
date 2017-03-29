91887c8d65da5a7d674426f61f7958ccb136a8d7
swl <swl@inam>
2017-03-29 15:15:21 +0700

    29/03/2560

--- common/config/main.php
+++ common/config/main.php
@@ -4,6 +4,15 @@ return [
     'name'=>'REPORT-BK',
     'language'=>'th_TH',
     'components' => [
+        'user' => [
+            'identityClass' => 'dektrium\user\models\User',
+            'enableAutoLogin' => true,
+            'identityCookie' => ['name' => '_identity-frontend', 'httpOnly' => true],
+        ],
+        'authManager' => [
+            'class' => 'dektrium\rbac\components\DbManager',
+        ],
+        
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
@@ -14,6 +23,32 @@ return [
         ],
         'gridview' =>  [
         'class' => '\kartik\grid\Module'
-    ]
+    ],
+        'user' => [
+        'class' => 'dektrium\user\Module',
+        'enableUnconfirmedLogin' => true,
+        'confirmWithin' => 21600,
+        'cost' => 12,
+        'admins' => ['admin']
+    ],
+        'rbac' => 'dektrium\rbac\RbacWebModule',
+        'admin' => [
+            'class' => 'mdm\admin\Module',
+            'layout'=>'left-menu'
+            
+        ]
+    ],
+    'as access' => [
+        'class' => 'mdm\admin\components\AccessControl',
+        'allowActions' => [
+            'site/*',
+            'admin/*',
+            'some-controller/some-action',
+            // The actions listed here will be allowed to everyone including guests.
+            // So, 'admin/*' should not appear here in the production, of course.
+            // But in the earlier stages of your development, you may probably want to
+            // add a lot of actions here until you finally completed setting up rbac,
+            // otherwise you may not even take a first step.
+        ]
     ],
 ];
